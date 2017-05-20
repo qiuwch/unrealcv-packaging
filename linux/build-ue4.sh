21d5da140c09d3c8b346dd7852f47d1a96885b6a
Weichao Qiu <qiuwch@gmail.com>
2017-05-19 22:24:09 -0400

    Update linux packaging scripts.

--- linux/build-ue4.sh
+++ linux/build-ue4.sh
@@ -1,1 +1,5 @@
-docker run -it --rm -v ${PWD}/UnrealEngine:/UE4 unrealcv/ue4-dependency bash -c "sudo chown -R unrealcv /UE4; cd UE4; sudo apt-get update; ./Setup.sh; ./GenerateProjectFiles.sh; make -C /UE4"
+if [ ! -d ./UnrealEngine ]
+then
+    echo "Use git clone to download UnrealEngine source code first."
+fi
+docker run -it --rm -v ${PWD}/UnrealEngine:/UE4 qiuwch/ue4-base bash -c "sudo chown -R unrealcv /UE4; cd UE4; sudo apt-get update; ./Setup.sh; ./GenerateProjectFiles.sh; make -C /UE4"
