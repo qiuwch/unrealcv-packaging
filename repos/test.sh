f6d7cccb5d2f3a8a589f5a8484814e0a175bcafa
Weichao Qiu <qiuwch@gmail.com>
2017-05-20 00:28:19 -0400

    Add scripts to automate cloning repos.

---
+++ repos/test.sh
@@ -0,0 +1,10 @@
+# `git lfs` is required to access these repos. They are private because we are restricted by the licenses of the digital content authors.
+
+# https://github.com/qiuwch/UE4VirtualArm
+git clone https://github.com/qiuwch/UE4RealisticRendering
+# https://github.com/qiuwch/UE414Minimal
+# https://github.com/qiuwch/UE4Keypoint
+# https://github.com/qiuwch/UE4UrbanCity
+# https://github.com/qiuwch/UE4ArchinteriorsVol2Scene1
+# https://github.com/qiuwch/UE4ArchinteriorsVol2Scene2
+# https://github.com/qiuwch/UE4ArchinteriorsVol2Scene3  
