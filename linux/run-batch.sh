e304a36ddd29756ff529a4ce5c7bca174c7a1cfb
Weichao Qiu <qiuwch@gmail.com>
2017-05-20 02:33:40 -0400

    Add run-batch to compile all projects.

---
+++ linux/run-batch.sh
@@ -0,0 +1,6 @@
+package.sh ../repos/UE414Minimal/UE414Minimal.uproject
+package.sh ../repos/UE4ArchinteriorsVol2Scene1/ArchinteriorsVol2Scene1.uproject
+package.sh ../repos/UE4ArchinteriorsVol2Scene2/ArchinteriorsVol2Scene2.uproject
+package.sh ../repos/UE4ArchinteriorsVol2Scene3/ArchinteriorsVol2Scene3.uproject
+package.sh ../repos/UE4RealisticRendering/RealisticRendering.uproject
+package.sh ../repos/UE4UrbanCity/UrbanCity.uproject
