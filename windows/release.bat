e4e196e18d57ea0bad2082c4845b08965c7cd14b
Weichao Qiu <qiuwch@gmail.com>
2017-05-19 22:28:00 -0400

    Update the path of python scripts.

--- windows/release.bat
+++ windows/release.bat
@@ -2,6 +2,6 @@ set build_name=%1%
 rem Remove the trailing slash
 if %build_name:~-1%==\ set build_name=%build_name:~0,-1%
 rem Package contents into a zip file
-python ..\zip.py %build_name%.zip %build_name%\WindowsNoEditor
+python ..\common\zip.py %build_name%.zip %build_name%\WindowsNoEditor
 rem Upload binary
-python ..\gdrive_upload.py %build_name%.zip
+python ..\common\gdrive_upload.py %build_name%.zip
