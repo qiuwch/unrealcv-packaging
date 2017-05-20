ee7561e0040bca21432bcf68721afb64216d053e
Weichao Qiu <qiuwch@gmail.com>
2017-05-20 18:48:58 -0400

    Change gdrive to scp.

--- linux/release.sh
+++ linux/release.sh
@@ -3,4 +3,5 @@ echo Remove the trailing slash
 echo Package contents into a zip file
 python ../common/zip.py ${build_name}.zip ${build_name}/LinuxNoEditor
 echo Upload binary
-python ../common/gdrive_upload.py ${build_name}.zip
+# python ../common/gdrive_upload.py ${build_name}.zip
+scp ${build_name}.zip qiuwch@gradx.cs.jhu.edu:/users/qiuwch/public_html/release/unrealcv/
