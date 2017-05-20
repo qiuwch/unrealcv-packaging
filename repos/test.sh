c42705bb276805e2d33a310c742616ca2988c8ee
Weichao Qiu <qiuwch@gmail.com>
2017-05-20 02:15:46 -0400

    Add more repos.

--- repos/test.sh
+++ repos/test.sh
@@ -1,10 +1,12 @@
 # `git lfs` is required to access these repos. They are private because we are restricted by the licenses of the digital content authors.
 
 # https://github.com/qiuwch/UE4VirtualArm
+git config --global credential.helper 'cache --timeout=3600'
+# Make sure password is cached
 git clone https://github.com/qiuwch/UE4RealisticRendering
 # https://github.com/qiuwch/UE414Minimal
 # https://github.com/qiuwch/UE4Keypoint
-# https://github.com/qiuwch/UE4UrbanCity
-# https://github.com/qiuwch/UE4ArchinteriorsVol2Scene1
-# https://github.com/qiuwch/UE4ArchinteriorsVol2Scene2
-# https://github.com/qiuwch/UE4ArchinteriorsVol2Scene3  
+git clone https://github.com/qiuwch/UE4UrbanCity
+git clone https://github.com/qiuwch/UE4ArchinteriorsVol2Scene1
+git clone https://github.com/qiuwch/UE4ArchinteriorsVol2Scene2
+git clone https://github.com/qiuwch/UE4ArchinteriorsVol2Scene3  
