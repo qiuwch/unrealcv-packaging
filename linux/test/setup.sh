e52516d774c780fb50aa317801f6500e8ce1a0ef
Weichao Qiu <qiuwch@gmail.com>
2017-05-30 02:27:16 -0400

    Fix some path issue.

--- linux/test/setup.sh
+++ linux/test/setup.sh
@@ -1,9 +1,10 @@
 # All all binaries need to be tested.
 url=$1
-name=$(echo ${url} | sed 's/.*\/\(.*\)-Linux.*/\1/')
+build_name=$(echo ${url} | sed 's/.*\/\(.*\).zip/\1/')
+# name=$(echo ${url} | sed 's/.*\/\(.*\)-Linux.*/\1/')
 zip_file=$(echo ${url} | sed 's/.*\/\(.*-Linux.*zip\)/\1/')
 echo ${name}
-mkdir ${name}
-cd ${name}
+# mkdir ${build_name}
+# cd ${build_name}
 wget -c ${url}
 unzip ${zip_file}
