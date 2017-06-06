98f317c6d3b55b70ce8471abed1d77ccd11984d1
Weichao Qiu <qiuwch@gmail.com>
2017-06-05 22:01:51 -0400

    Add packaging README, fix an absolute path issue.

--- windows/package.bat
+++ windows/package.bat
@@ -1,5 +1,5 @@
 call config.bat
-set project=%1%
+set project=%~f1%
 rem Available options in here http://stackoverflow.com/questions/659647/how-to-get-folder-path-from-file-path-with-cmd
 set project_folder=%~dp1%
 set project_name=%~n1%
@@ -16,9 +16,9 @@ if not exist %project% (
 
 set cmd="%UE4%\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project=%project% -archivedirectory=%output_folder% -noP4 -platform=Win64 -clientconfig=Development -serverconfig=Development -allmaps -stage -pak -archive -cook -build
 if exist %project_folder%\Source (
-	rem Build source code 
+	rem Build source code
     set cmd=%cmd% -build
-) 
+)
 %cmd%
 
 call release.bat %build_name%
