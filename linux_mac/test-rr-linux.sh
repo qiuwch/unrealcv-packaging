13449f034ab4da6af32a3c201b71a68dff99600d
Weichao Qiu <qiuwch@gmail.com>
2017-06-11 17:40:20 -0400

    Update test scripts.

--- linux_mac/test-rr-linux.sh
+++ linux_mac/test-rr-linux.sh
@@ -1,6 +1,10 @@
-./RealisticRendering-Linux-0.3.8/LinuxNoEditor/RealisticRendering/Binaries/Linux/RealisticRendering &
+./RealisticRendering-Linux-0.3.8/LinuxNoEditor/RealisticRendering/Binaries/Linux/RealisticRendering > rr.log  &
 pid=$!
 sleep 2
 
+pytest unrealcv/test/connection_test.py
+pytest unrealcv/test/camera_test.py
+pytest unrealcv/test/object_test.py
+pytest unrealcv/test/stereo_test.py
 pytest unrealcv/test/rr_test.py
 kill ${pid}
--- linux_mac/test-rr-mac.sh
+++ linux_mac/test-rr-mac.sh
@@ -3,10 +3,10 @@
 open RealisticRendering-Darwin-0.3.8/MacNoEditor/RealisticRendering.app
 sleep 2
 
-# pytest unrealcv/test/connection_test.py
-# pytest unrealcv/test/camera_test.py
-# pytest unrealcv/test/object_test.py
-# pytest unrealcv/test/stereo_test.py
+pytest unrealcv/test/connection_test.py
+pytest unrealcv/test/camera_test.py
+pytest unrealcv/test/object_test.py
+pytest unrealcv/test/stereo_test.py
 pytest unrealcv/test/rr_test.py
 
 pkill RealisticRendering
