43515be4dd08627302f06d93eca6fbc08ab0b74d
Weichao Qiu <qiuwch@gmail.com>
2017-06-12 22:43:15 -0400

    Fix minor issue.

--- common/build-conf.py
+++ common/build-conf.py
@@ -8,16 +8,19 @@ def parse_unrealcv_version(unrealcv_folder='./unrealcv'):
 
 def parse_ue4_version(ue4_folder=os.environ['UE4']):
     version_file = os.path.join(ue4_folder, 'Engine/Build/Build.version')
+    if not os.path.isfile(version_file):
+        return None
+
     with open(version_file) as f:
         v = json.load(f)
     ue4_version = '%s.%s' % (v['MajorVersion'], v['MinorVersion']) # PathVersion is not neccessary
     return ue4_version
 
 def parse_platform():
-    return platform.system() 
+    return platform.system()
 
 if __name__ == '__main__':
-    unrealcv_version = parse_unrealcv_version() 
+    unrealcv_version = parse_unrealcv_version()
     ue4_version = parse_ue4_version()
     info = dict(
         unrealcv_version = unrealcv_version,
@@ -29,7 +32,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     format_help = '''The information to be included in the string, available options are %s
     An example is "{ue4_version}-{unrealcv_version}''' % info_keys
-    parser.add_argument('--format', required=True, help = format_help) 
+    parser.add_argument('--format', required=True, help = format_help)
     args = parser.parse_args()
 
     print(args.format.format(**info))
