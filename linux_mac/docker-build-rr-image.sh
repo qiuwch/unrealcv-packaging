de7eedc4f33b8a09ae65d5ffd237512960549137
Weichao Qiu <qiuwch@gmail.com>
2017-06-12 22:43:50 -0400

    Organize docker image packaging script.

--- linux_mac/docker-build-rr-image.sh
+++ linux_mac/docker-build-rr-image.sh
@@ -1,17 +1,19 @@
 # Download the latelest version of RR binary.
-version=0.3.6
-url=http://cs.jhu.edu/~qiuwch/release/unrealcv/RealisticRendering-Linux-${version}.zip
-wget -c ${url}
+export version=$(python ../common/build-conf.py --format "{unrealcv_version}")
 
 # Parse the filename to get the version of the UnrealCV version.
 # version=$(echo ${url} | sed 's/.*\/Linux-\(.*\).zip/\1/')
 echo ${version}
 
 # # Pull the basic nvidia docker image.
-# docker pull qiuwch/nvidia
+docker pull qiuwch/nvidia
+
+rr_folder=RealisticRendering-Linux-${version}
+cp Dockerfile ${rr_folder}
+# Do the build in the rr_folder to avoid the very large context files
 
 # Run and commit the docker image
-docker build . -t qiuwch/rr:${version}
+docker build --build-arg unrealcv_version=${version} ${rr_folder} -t qiuwch/rr:${version}
 
 # Push this docker image to the dockerhub.
 docker push qiuwch/rr:${version}
